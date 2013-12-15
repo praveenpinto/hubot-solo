be145359a61dbc26523bd3ee3018f6fbd6b36c30
JJ Asghar <jj.asghar@peopleadmin.com>
2013-12-15 15:54:07 -0600

    started campfire and irc plugins

--- recipes/campfire.rb
+++ recipes/campfire.rb
@@ -1,9 +1,8 @@
 #
-# main default recipe, this is for testing only!
+# campfire recipe
 #
 
 include_recipe "hubot-solo::nodejs"
-include_recipe "hubot-solo::personal_git_hubot"
 include_recipe "hubot-solo::official_git_hubot"
 include_recipe "hubot-solo::foreverjs"
 include_recipe "hubot-solo::redis"
@@ -14,8 +13,16 @@ bash "run myhubot" do
   creates "maybe"
   code <<-EOH
   STATUS=0
-  su vagrant -c bin/hubot --help || STATUS=1
+  su vagrant -c \
+   forever start -l forever.log -o out.log -e err.log
+   HUBOT_CAMPFIRE_ACCOUNT="some account"
+   HUBOT_CAMPFIRE_TOKEN="some token"
+   HUBOT_CAMPFIRE_ROOMS="666,333"
+   bin/hubot -a campfire --name Hubot || STATUS=1
   exit $STATUS
   EOH
 end
+
+# stop the forever via
+# forever stop bin/hubot
 
--- recipes/irc.rb
+++ recipes/irc.rb
@@ -1,12 +1,24 @@
 #
-# main default recipe, this is for testing only!
+# irc recipe
 #
 
 include_recipe "hubot-solo::nodejs"
-include_recipe "hubot-solo::personal_git_hubot"
 include_recipe "hubot-solo::official_git_hubot"
 include_recipe "hubot-solo::foreverjs"
 include_recipe "hubot-solo::redis"
+
+bash "install hubot-irc" do
+  user "vagrant"
+  cwd "/home/vagrant/repo/hubot"
+  creates "maybe"
+  code <<-EOH
+  STATUS=0
+  su vagrant -c npm install hubot-irc --save || STATUS=1
+  su vagrant -c npm install || STATUS=1
+  exit $STATUS
+  EOH
+end
+
 
 bash "run myhubot" do
   user "vagrant"
@@ -14,8 +26,17 @@ bash "run myhubot" do
   creates "maybe"
   code <<-EOH
   STATUS=0
-  su vagrant -c bin/hubot --help || STATUS=1
+  su vagrant -c \
+   forever start -l forever.log -o out.log -e err.log
+   HUBOT_IRC_SERVER=irc.freenode.net \
+   HUBOT_IRC_ROOMS="#jjtesting-irc" \
+   HUBOT_IRC_NICK="jjhubot-testing" \
+   HUBOT_IRC_UNFLOOD="true" \
+   bin/hubot -a irc --name jjhubot-testing || STATUS=1
   exit $STATUS
   EOH
 end
+
+# stop the forever via
+# forever stop bin/hubot
 
